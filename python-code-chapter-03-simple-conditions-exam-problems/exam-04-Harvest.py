377d130c8be2842539bc74a2bc1c376b775db15c
Tanya Evtimova <tanya_evt@yahoo.co.uk>
2018-07-01 08:56:44 +0300

    Removed 4 extra brackets from the code

--- python-code-chapter-03-simple-conditions-exam-problems/exam-04-Harvest.py
+++ python-code-chapter-03-simple-conditions-exam-problems/exam-04-Harvest.py
@@ -11,10 +11,10 @@ vine = harvestPerVine / 2.5
 if vine >= neededLiters:
     vineLeft = vine - neededLiters
     print('Good harvest this year! Total wine: {0} liters.' . format(
-        math.floor((vine))))
+        math.floor(vine)))
     print('{0} liters left -> {1} liters per person.' . format(
         math.ceil(vineLeft), math.ceil(vineLeft/workers)))
 else:
     print('It will be a tough winter! More {0} liters wine needed.'.format(
-        math.floor((neededLiters - vine))))
+        math.floor(neededLiters - vine)))
 
